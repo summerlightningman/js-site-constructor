dc792f38681bb77d80dbb88518eab1c7b504e481
summerlightningman <div8@bk.ru>
2020-12-11 10:34:53 +0300

    added bootstrap as package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {Site} from "./classes/site";
 import './styles/style.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import {model} from "./model";
 
 const site = new Site('#site');
