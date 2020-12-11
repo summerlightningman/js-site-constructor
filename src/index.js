cc699a39bddb7761229861cbea71870c4fa35405
summerlightningman <div8@bk.ru>
2020-12-11 11:37:34 +0300

    added class App

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import {Site} from "./classes/site";
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import {model} from "./model";
-import {Sidebar} from "./classes/sidebar";
+import {App} from "./classes/app";
 
-const site = new Site('#site');
-site.render(model);
-
-const updateCallback = newBlock => {
-    model.push(newBlock);
-    site.render(model);
-};
-
-new Sidebar('#panel', updateCallback);
+new App(model).init();
